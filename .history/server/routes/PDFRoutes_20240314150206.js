5bb18f49687f931302b073a7897c6b2f067fa1d0
drumsmanku <mayank.tanwar4444@gmail.com>
2024-03-14 15:03:44 +0530

    final commit

--- .history/server/routes/PDFRoutes_20240314150206.js
+++ .history/server/routes/PDFRoutes_20240314150206.js
@@ -4,29 +4,8 @@ const router = express.Router();
 const cors = require('cors');
 router.use(cors());
 
-router.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "http://localhost:5173"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
-
-  next();
-});
 
 router.post('/generate-pdf', async (req, res) => {
-  
 
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'POST');
