b92b6276628ae9a1f24522cbdf16698dcbe5b4c8
drumsmanku <mayank.tanwar4444@gmail.com>
2024-03-14 16:08:57 +0530

    final push

---
+++ .history/server/routes/PDFRoutes_20240314160840.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const router = express.Router();
+const cors = require('cors');
+const { generatePdf } = require('html-pdf-node');
+router.use(cors());
+
+
+router.post('/generate-pdf', async (req, res) => {
+  try {
+    const { htmlContent } = req.body;
+    
+    // Options for the PDF generation
+    let options = { format: 'A4' };
+    
+    // Generate PDF from the HTML content
+    let file = { content: htmlContent };
+    generatePdf(file, options).then(pdfBuffer => {
+      // Send the PDF as a response
+      res.contentType('application/pdf');
+      res.send(pdfBuffer);
+    });
+  } catch (error) {
+    console.error('Error generating PDF:', error);
+    res.status(500).send('Error generating PDF');
+  }
+});
+module.exports=router
