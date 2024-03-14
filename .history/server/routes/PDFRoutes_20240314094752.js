5b9bbbf7a7c7d78479f8c6c446daebcd607e3d7e
drumsmanku <mayank.tanwar4444@gmail.com>
2024-03-14 09:49:06 +0530

    final commit

--- .history/server/routes/PDFRoutes_20240314094752.js
+++ .history/server/routes/PDFRoutes_20240314094752.js
@@ -10,12 +10,12 @@ router.post('/generate-pdf', async (req, res) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'POST');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-  const { htmlContent } = req.body; 
+  const { products } = req.body; 
   await page.setBypassCSP(true)
   const browser = await puppeteer.launch({ headless: false, args: [ '--disable-gpu', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-sandbox', '--disable-web-security', '--disable-features=IsolateOrigins', '--disable-site-isolation-trials', '--disable-features=BlockInsecurePrivateNetworkRequests', ], devtools: true, defaultViewport: { hasTouch: true, isMobile: true, height: 1080, width: 1920, }, });
   const page = await browser.newPage();
   
-  await page.setContent(htmlContent, { waitUntil: 'networkidle0' }); 
+  await page.setContent(products, { waitUntil: 'networkidle0' }); 
   await page.waitForSelector('.black-back');
   
   const pdfBuffer = await page.pdf({ format: 'A4' });
